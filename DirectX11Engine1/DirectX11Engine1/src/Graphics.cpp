55a6e3b1cd0b980b1aacf3d5722e0800dd9f025b
Dem4236 <onoderadesk0402@gmail.com>
2020-10-12 01:13:55 +0900

    chapter17 インデックスバッファを作成し、六角形を描画

--- DirectX11Engine1/DirectX11Engine1/src/Graphics.cpp
+++ DirectX11Engine1/DirectX11Engine1/src/Graphics.cpp
@@ -119,19 +119,9 @@ void Graphics::DrawTriangle()
 		{0.0f,0.5f,255,0,0,0},
 		{0.5f,-0.5f,0,255,0,0},
 		{-0.5f,-0.5f,0,0,255,0},
-
-		{0.0f,0.5f,255,0,0,0},
-		{-0.5f,-0.5f,0,255,0,0},
 		{-0.3f,0.3f,0,0,255,0},
-
-		{0.0f,0.5f,255,0,0,0},
 		{0.3f,0.3f,0,255,0,0},
-		{0.5f,-0.5f,0,0,255,0},
-
 		{0.0f,-0.8f,255,0,0,0},
-		{-0.5f,-0.5f,0,255,0,0},
-		{0.5f,-0.5f,0,0,255,0},
-
 	};
 
 	//頂点バッファの作成
@@ -147,6 +137,28 @@ void Graphics::DrawTriangle()
 	D3D11_SUBRESOURCE_DATA sd = {};
 	sd.pSysMem = vertices;
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
+
+	//インデックスバッファの作成
+	const unsigned short indices[] = {
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	pDeviceContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+
 
 	//パイプラインにバインド
 	const UINT stride = sizeof(Vertex);
@@ -205,5 +217,5 @@ void Graphics::DrawTriangle()
 	//設定
 	pDeviceContext->RSSetViewports(1u, &vp);
 
-	GFX_THROW_INFO_ONLY(pDeviceContext->Draw((UINT)std::size(vertices), 0u));
+	GFX_THROW_INFO_ONLY(pDeviceContext->DrawIndexed((UINT)std::size(indices),0u, 0u));
 }
